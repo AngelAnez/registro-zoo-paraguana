cb0e32aab3ed7cb20b8f2b941f5ce445fc2d9be5
Angel Añez <angdavid2910@gmail.com>
2024-02-20 16:00:11 -0400

    Implementación de la lógica y notificaciones en la vista Ajustes

--- src/controllers/ajustes.controller.js
+++ src/controllers/ajustes.controller.js
@@ -1,8 +1,77 @@
 import { getUserSession } from "../models/userSession.js";
+import { getConfig, postConfig } from "../models/config.js";
 
-export const renderAjustes = (req, res) => {
+export const getAjustes = (req, res) => {
+  renderAjustes(res, {showAlert: false, messageAlert: "", typeAlert: ""});
+};
+
+export const renderAjustes = (res, alert) => {
   const user = getUserSession();
+  const {
+    internetDolarValue,
+    defaultDolarValue,
+    childTicketPrice,
+    adultTicketPrice,
+    olderTicketPrice,
+  } = getConfig();
+
+  const {showAlert, messageAlert, typeAlert} = alert
+
   res.render("ajustes", {
     username: user.username,
+    internetDolarValue,
+    defaultDolarValue,
+    childTicketPrice,
+    adultTicketPrice,
+    olderTicketPrice,
+    showAlert,
+    messageAlert,
+    typeAlert
   });
+};
+
+export const changeConfig = (req, res) => {
+  const {
+    internetDolarValue,
+    defaultDolarValue,
+    childTicketPrice,
+    adultTicketPrice,
+    olderTicketPrice,
+  } = req.body;
+
+  let configData = {
+    internetDolarValue: internetDolarValue ?? "off",
+    defaultDolarValue,
+    childTicketPrice,
+    adultTicketPrice,
+    olderTicketPrice,
+  };
+
+  const internetDolarRegex = /^(on|off)$/;
+  const numberRegex = /^[0-9]+([.][0-9]+)?$/;
+
+  if (!internetDolarRegex.test(configData.internetDolarValue)) {
+    return renderAjustes(res, {showAlert: true, messageAlert: "Se ha producido un error al momento de guardar los cambios", typeAlert: "danger"});
+  }
+
+  if (!numberRegex.test(configData.defaultDolarValue)) {
+    return renderAjustes(res, {showAlert: true, messageAlert: "Se ha producido un error al momento de guardar los cambios", typeAlert: "danger"});
+  }
+
+  if (!numberRegex.test(configData.childTicketPrice)) {
+    return renderAjustes(res, {showAlert: true, messageAlert: "Se ha producido un error al momento de guardar los cambios", typeAlert: "danger"});
+  }
+
+  if (!numberRegex.test(configData.adultTicketPrice)) {
+    return renderAjustes(res, {showAlert: true, messageAlert: "Se ha producido un error al momento de guardar los cambios", typeAlert: "danger"});
+  }
+
+  if (!numberRegex.test(configData.olderTicketPrice)) {
+    return renderAjustes(res, {showAlert: true, messageAlert: "Se ha producido un error al momento de guardar los cambios", typeAlert: "danger"});
+  }
+
+  configData = JSON.stringify(configData);
+  postConfig(configData);
+
+  renderAjustes(res, {showAlert: true, messageAlert: "Los cambios han sido guardados exitosamente", typeAlert: "success"});
 };
--- src/routes/ajustes.routes.js
+++ src/routes/ajustes.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { renderAjustes } from "../controllers/ajustes.controller.js";
+import { getAjustes, changeConfig} from "../controllers/ajustes.controller.js";
 import { userConnected } from "../middlewares/userConnected.js";
 
 const router = Router()
 
-router.get("/ajustes", userConnected, renderAjustes)
+router.get("/ajustes", userConnected, getAjustes)
+
+router.post("/ajustes", userConnected, changeConfig)
 
 export default router
