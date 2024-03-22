0c9549d13dcadac52e8d691c428bd0c9a87fe074
Angel Añez <angdavid2910@gmail.com>
2024-03-22 17:58:59 -0400

    Agregado el Backend de la vista Perfil

--- src/controllers/perfil.controller.js
+++ src/controllers/perfil.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import { createAccessToken } from "../lib/jwt.js";
+import bcryptjs from "bcryptjs";
 
 export const getPerfil = async (req, res) => {
   const {username, admin} = req.user;
@@ -8,6 +10,63 @@ export const getPerfil = async (req, res) => {
   res.render("perfil", {
     username,
     admin,
-    email: actualUser.email
+    email: actualUser.email,
+    showAlert: false,
+    messageAlert: "",
+    typeAlert: ""
   });
 };
+
+export const changePerfil = async (req, res) => {
+  let {username, admin} = req.user
+  const {newUsername, email, actualPassword, newPassword} = req.body
+
+  const originalUser = await User.findOne({ username });
+
+  if (newUsername && email){
+    if (newUsername != username){
+      const userRepeatedDb = await User.findOne({ username: newUsername });
+      if (userRepeatedDb){
+        return res.render("perfil", {
+          username,
+          admin,
+          email: originalUser.email,
+          showAlert: true,
+          messageAlert: "El nombre de usuario que ha ingresado ya existe",
+          typeAlert: "danger"
+        });
+      }
+      const token = await createAccessToken({username: newUsername, admin });
+      res.cookie("token", token);
+    }
+    await User.findOneAndUpdate({username}, {username: newUsername, email})
+    username = newUsername
+  }
+
+  if (actualPassword && newPassword){
+    const validPassword = await bcryptjs.compare(actualPassword, originalUser.password);
+    if (!validPassword) {
+        return res.render("perfil", {
+          username,
+          admin,
+          email: originalUser.email,
+          showAlert: true,
+          messageAlert: "La contraseña que ha ingresado es incorrecta",
+          typeAlert: "danger"
+      });
+    }
+    const passwordHash = await bcryptjs.hash(newPassword, 10);
+    await User.updateOne({username}, {password: passwordHash})
+  }
+  
+  const userModified = await User.findOne({username})
+
+  res.render("perfil", {
+    username,
+    admin,
+    email: userModified.email,
+    showAlert: true,
+    messageAlert: "Los cambios han sido realizados exitosamente",
+    typeAlert: "success"
+  });
+}
--- src/routes/perfil.routes.js
+++ src/routes/perfil.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { getPerfil } from "../controllers/perfil.controller.js"
+import { changePerfil, getPerfil } from "../controllers/perfil.controller.js"
 import { validateToken } from "../middlewares/validateToken.js";
 
 const router = Router()
 
 router.get("/perfil", validateToken, getPerfil)
+
+router.post("/perfil", validateToken, changePerfil)
 
 export default router
