e9a80034610b503c30af2a07146a82f62e6b29b7
Angel Añez <angdavid2910@gmail.com>
2025-08-25 22:00:57 -0400

    env: add jwt secret to env variables

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,20 +1,26 @@
 import jwt from "jsonwebtoken";
-import { TOKEN_SECRET_KEY } from "../global.js";
+
+const JWT_SECRET = process.env.TOKEN_SECRET_KEY;
 
 export const createAccessToken = (payload) => {
-    return new Promise((resolve, reject) => {
-        jwt.sign(payload, TOKEN_SECRET_KEY, {
-        expiresIn: "10h"
-        }, (err, token) => {
-            if (err) reject()
-            resolve(token)
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      JWT_SECRET,
+      {
+        expiresIn: "10h",
+      },
+      (err, token) => {
+        if (err) reject();
+        resolve(token);
+      }
+    );
+  });
+};
 
 export const verifyAccessToken = (token) => {
-    return jwt.verify(token, TOKEN_SECRET_KEY, (err, user) => {
-        if (err) return false
-        return user
-    })
-}
+  return jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return false;
+    return user;
+  });
+};
