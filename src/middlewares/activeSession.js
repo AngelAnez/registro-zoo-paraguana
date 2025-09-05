925660455208766eeea6e015cc7ff79035794a2b
Angel Añez <angdavid2910@gmail.com>
2025-09-04 20:22:02 -0400

    style: lint middlewares

--- src/middlewares/activeSession.js
+++ src/middlewares/activeSession.js
@@ -1,10 +1,10 @@
-import {verifyAccessToken} from "../libs/jwt.js";
+import { verifyAccessToken } from "../libs/jwt.js";
 
 export const activeSession = (req, res, next) => {
-  const {token} = req.cookies
-    if (token && verifyAccessToken(token)){
-      req.user = verifyAccessToken(token)
-      return res.redirect("/home");        
-    }
-    next();
+  const { token } = req.cookies;
+  if (token && verifyAccessToken(token)) {
+    req.user = verifyAccessToken(token);
+    return res.redirect("/home");
+  }
+  next();
 };
